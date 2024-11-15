2e18e5283fe6971626551dee5093c6e7f93b7f22
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-11-15 12:59:46 -0300

    feat: get game by id

--- src/main/java/com/abcempresa/movielist/services/GameService.java
+++ src/main/java/com/abcempresa/movielist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.abcempresa.movielist.dto.GameDTO;
 import com.abcempresa.movielist.dto.GameMinDTO;
 import com.abcempresa.movielist.entities.Game;
 import com.abcempresa.movielist.repositories.GameRepository;
@@ -15,8 +17,15 @@ public class GameService {
 	@Autowired //injecao de instancia do repositorio
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true) //anotação para que a ação respeite o modelo ACID das transações
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
